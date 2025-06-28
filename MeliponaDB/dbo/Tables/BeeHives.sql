28fd385965a328faafca1a4d3289c207109a8562
Alejoho <alejoreilly96@gmail.com>
2025-06-28 07:35:55 -0400

    Extract some data out of the BeeHives table to the new Nests table

--- MeliponaDB/dbo/Tables/BeeHives.sql
+++ MeliponaDB/dbo/Tables/BeeHives.sql
@@ -1,10 +1,6 @@
 CREATE TABLE [dbo].[BeeHives]
 (
   [Id] INT NOT NULL PRIMARY KEY IDENTITY,
-  [FirstNest] INT NOT NULL,
-  [SecondNest] INT,
-  [ThirdNest] INT,
-  [FourthNest] INT,
   [CreationDate] DATE,
   [DivisionDate] DATE
 )
---
+++ MeliponaDB/dbo/Tables/Nests.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Nests]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[BeeHiveId] INT NOT NULL, 
+    [NestNumber] INT NOT NULL, 
+    [PlacementDate] DATE NOT NULL, 
+    [EmptyAtPlacement] BIT NOT NULL DEFAULT 1, 
+    CONSTRAINT [FK_Nests_BeeHives] FOREIGN KEY ([BeeHiveId]) REFERENCES [BeeHives]([Id]),
+)
