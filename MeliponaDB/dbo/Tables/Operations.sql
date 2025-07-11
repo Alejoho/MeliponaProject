4a6f075a99f65af8f954676a5653402bf48d6f7b
Alejoho <alejoreilly96@gmail.com>
2025-07-11 14:21:08 -0400

    Make a change in the Operations table

--- MeliponaDB/dbo/Tables/Operations.sql
+++ MeliponaDB/dbo/Tables/Operations.sql
@@ -2,7 +2,7 @@ CREATE TABLE [dbo].[Operations]
 (
   [Id] INT NOT NULL PRIMARY KEY IDENTITY,
   [BeeHiveId] INT NOT NULL,
-  [Date] DATE NOT NULL,
+  [Date] DATETIME2 NOT NULL,
   [Description] NVARCHAR(500) NOT NULL,
   CONSTRAINT FK_Operations_BeeHives_Id FOREIGN KEY (BeeHiveId) REFERENCES [dbo].[BeeHives](Id)
 )
